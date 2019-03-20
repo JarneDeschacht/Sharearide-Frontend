ea5709cbf357e4f6bce911d2cb9894e222bb6c57
Jarne Deschacht <jarne.deschacht@student.hogent.be>
2019-03-20 13:13:22 +0100

    create angular project and create first component

---
+++ Sharearide-Frontend/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RideComponent } from './ride/ride.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RideComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
