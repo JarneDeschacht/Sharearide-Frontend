5fc08fe8296b8b5e02ff13550591900d3a8d9a13
Jarne Deschacht <jarne.deschacht@student.hogent.be>
2019-05-01 23:17:31 +0200

    trying to deploy the app

--- Sharearide-Frontend/server.js
+++ Sharearide-Frontend/server.js
@@ -1,14 +1,16 @@
+//Install express server
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
-// Serve static files....
-app.use(express.static(__dirname + '/dist/sharearideapplication'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/Sharearide-Frontend'));
 
-// Send all requests to index.html
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/sharearideapplication/index.html'));
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/Sharearide-Frontend/index.html'));
 });
 
-// default Heroku PORT
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
