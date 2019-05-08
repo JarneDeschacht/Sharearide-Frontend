eaf9144e2df18ccb111bb7fef668f5b3ca822923
Jarne Deschacht <jarne.deschacht@student.hogent.be>
2019-05-08 16:59:42 +0200

    some small changes

--- Sharearide-Frontend/src/app/profile/user/personal-data/personal-data.component.ts
+++ Sharearide-Frontend/src/app/profile/user/personal-data/personal-data.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Validators, FormBuilder, FormGroup, FormControl} from '@angular/forms';
+import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { SharearideDataService } from 'src/app/dataservice/sharearide-data.service';
 import { Gender } from 'src/app/account/account.component';
@@ -41,8 +41,8 @@ export class PersonalDataComponent implements OnInit {
       lastname: new FormControl(this.user.lastName, [Validators.required]),
       gender: new FormControl(this.user.gender, [Validators.required]),
       telnr: new FormControl(this.user.phoneNumber.substring(1), [Validators.required, Validators.pattern('^[0-9]{10,}')]),
-      borndate: new FormControl(this.user.dateOfBirth, [Validators.required,isValidDate]),
-      email: new FormControl(this.user.email, [Validators.required, Validators.email]),
+      borndate: new FormControl(this.user.dateOfBirth, [Validators.required, isValidDate]),
+      email: new FormControl({ value: `${this.user.email}`, disabled: true }, [Validators.required, Validators.email]),
     });
   }
 
@@ -91,9 +91,9 @@ export class PersonalDataComponent implements OnInit {
       return `Dit veld bevat geen geldig e-mailadres`;
     } else if (errors.pattern) {
       return `Geen geldig telefoonnummer`;
-    }else if(errors.InvalidBirthDate){
+    } else if (errors.InvalidBirthDate) {
       return 'Je moet minstens 18 jaar oud zijn';
-      }
+    }
   }
 
 }
--- Sharearide-Frontend/src/environments/environment.ts
+++ Sharearide-Frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://sharearide-backend.azurewebsites.net/api'
+  apiUrl: '/api'
 };
 
 /*
